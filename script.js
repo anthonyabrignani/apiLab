ffff1def888fa008d42692f9d75cfbdb87966bb8
anthony abrignani <anthony abrignani>
2021-04-26 18:16:07 -0400

    added styling

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const postParent = fetch("https://www.reddit.com/r/aww/.json").then((response) =>
-  response.json()
-);
+const postParent = fetch(
+  "https://www.reddit.com/r/aww/.json"
+).then((response) => response.json());
 
-postParent.then(data => {
-    const sectionElement = document.getElementById("main")
-    for (let postData of data.data.children) {
+postParent.then((data) => {
+  const sectionElement = document.getElementById("main");
+  for (let postData of data.data.children) {
+    // Making the title
+    const titleElement = document.createElement("h3");
+    titleElement.classList.add("post");
+    titleElement.innerText = postData.data.title;
+    sectionElement.appendChild(titleElement);
 
-        // Making the title
-        const titleElement = document.createElement("h3");
-        titleElement.classList.add("post")
-        titleElement.innerText = postData.data.title;
-        sectionElement.appendChild(titleElement);
+    // Making the images
+    const imageElement = document.createElement("img");
+    imageSource = postData.data.thumbnail;
+    imageElement.src = imageSource;
+    sectionElement.appendChild(imageElement);
 
-        // Making the images
-        const imageElement = document.createElement("img");
-        imageSource = postData.data.thumbnail;
-        imageElement.src = imageSource;
-        sectionElement.appendChild(imageElement);
+    const usernameElement = document.createElement("h4");
+    usernameElement.innerText = `Username: ${postData.data.author}  
+    Upvotes: ${postData.data.score}  
+    Rewards: ${postData.data.total_awards_received}  
+    Upvotes: ${postData.data.score}  `;
+    sectionElement.appendChild(usernameElement);
 
-        // Making the links
-        const linkElement = document.createElement("a");
-        linkSource = postData.data.url;
-        linkElement.setAttribute("href", linkSource);
-        linkElement.innerText = postData.data.url;
-        sectionElement.appendChild(linkElement);
-    }   
-})
-
+    // Making the links
+    const linkElement = document.createElement("a");
+    linkSource = postData.data.url;
+    linkElement.setAttribute("href", linkSource);
+    linkElement.innerText = postData.data.url;
+    sectionElement.appendChild(linkElement);
+  }
+});
