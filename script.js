827c838dfa1b6ba925436094a5ee9a4d3428b4a5
anthony abrignani <anthony abrignani>
2021-04-26 23:48:46 -0400

    added more styling

--- script.js
+++ script.js
@@ -5,30 +5,31 @@ const postParent = fetch(
 postParent.then((data) => {
   const sectionElement = document.getElementById("main");
   for (let postData of data.data.children) {
+      
     // Making the title
     const titleElement = document.createElement("h3");
     titleElement.classList.add("post");
     titleElement.innerText = postData.data.title;
     sectionElement.appendChild(titleElement);
 
-    // Making the images
-    const imageElement = document.createElement("img");
-    imageSource = postData.data.thumbnail;
-    imageElement.src = imageSource;
-    sectionElement.appendChild(imageElement);
-
-    const usernameElement = document.createElement("h4");
-    usernameElement.innerText = `Username: ${postData.data.author}  
-    Upvotes: ${postData.data.score}  
-    Rewards: ${postData.data.total_awards_received}  
-    Upvotes: ${postData.data.score}  `;
-    sectionElement.appendChild(usernameElement);
-
     // Making the links
     const linkElement = document.createElement("a");
     linkSource = postData.data.url;
     linkElement.setAttribute("href", linkSource);
-    linkElement.innerText = postData.data.url;
+    linkElement.innerText = "Click here to go to this post";
     sectionElement.appendChild(linkElement);
+
+    // Making the images
+    const imageElement = document.createElement("img");
+    imageSource = postData.data.thumbnail;
+    imageElement.src = imageSource;
+    imageElement.alt = postData.data.title;
+    sectionElement.appendChild(imageElement);
+
+    const usernameElement = document.createElement("h4");
+    usernameElement.innerText = ` ~ ~ Username: ${postData.data.author} ~ ~ Upvotes: ${postData.data.score} ~ ~ Rewards: ${postData.data.total_awards_received} ~ ~ NSFW: ${postData.data.over_18} ~ ~ `;
+    sectionElement.appendChild(usernameElement);
+
+    
   }
 });
